207ab8a7f2bfb387375356164b43e3102175b5ac
FuadAghayev <fuadagayevkc@gmail.com>
2018-09-17 20:16:50 -0500

    finlacommit

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+<head>
+<script src="jquery-3.3.1.min.js"></script>
+</head>
+
+<button onclick="setTimeout(myFunction, 3000)">Try it</button>
+
+//selector message to write based on score1
+var explode = function(){
+    alert("Win or Lose!");
+  };
+  setTimeout(explode, 2000);
+//function myFunction(); {
+    //alert('start');
+//}
+//var setTime = setTimeout( function ( ) { 
+  //javascript  
+//}
+<script type="text/javascript">
+        function countDown (secs.elem) {
+            var element = document.getElementById(elem);
+            element.innerHTML="you have "10 seconds";
+
+            secs--; 
+            var timer = setTimeout('countDown('+secs+',"'+elem+''")''
+        }
+    </script>
+    <div id="status"></div>
+    <script type="text/javascript">countDown(30,"status");</script>
+    
+
+ //when the player have true answer show true when player have wrong answer show false.
+if(answer = 4) 
+    document.write('you passed!');
+} else {
+    document.write('try again...');
+//}
+ alert(4); // true
+
+ //var world worldoceans=0; ); {
+     //document.write(worldoceans, 'four');
+    //worldhasfouroceans
+     //}
+
+
+//The player will have a limited amount of time to finish the quiz. 
+
+
+//The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+
+//Don't let the player pick more than one answer per question.
+//Don't forget to include a countdown timer.
